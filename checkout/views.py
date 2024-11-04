f66a6313bb363724da05086e99cba4e9783ccf90
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-11-04 11:53:07 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51QHOTHG2XPZVpqqYmtvF75Zj5hJQRY6E9qR576ryMwQPJNiUbFlq81s39OWhWQwFeSAKCl2seGcrkOhtvG1SvMtV00TfEBuuJp',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
