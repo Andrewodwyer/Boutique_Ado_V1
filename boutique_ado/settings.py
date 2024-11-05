c566949866e92c65e38545bb07c3e77766352b3a
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-11-05 15:30:25 +0000

    webhook added

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -183,4 +183,5 @@ load_dotenv()
 # Access your Stripe keys
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
+STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
