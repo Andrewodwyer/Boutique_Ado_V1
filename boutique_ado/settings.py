63a414e7b5f9aee6c6371a8a7f491aeb51e1df1d
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-11-12 20:37:06 +0000

    settings

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -213,7 +213,5 @@ STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 
-print('STRIPE_WH_SECRET', STRIPE_WH_SECRET)
-
 # prevent potential overflow issues with IDs in 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
