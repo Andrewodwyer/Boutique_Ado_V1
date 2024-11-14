3fdff8605570a7acde18ce71f4c87213746ec644
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-11-14 10:15:20 +0000

    404 page

--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from . import views
+from .views import handler404
 
 urlpatterns = [
     path('', views.index, name='home'),
 
 ]
+
+handler404 = 'boutique_ado.views.handler404'
