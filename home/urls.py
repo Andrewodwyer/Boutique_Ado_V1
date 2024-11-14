430a0a46abc4ac68157a1e2c424ee6e48d2a420e
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-11-14 10:22:23 +0000

    404 handler in wrong url before

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,7 @@
 from django.urls import path
 from . import views
-from .views import handler404
 
 urlpatterns = [
     path('', views.index, name='home'),
 
 ]
-
-handler404 = 'boutique_ado.views.handler404'
